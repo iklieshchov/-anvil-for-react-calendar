ca0817fbaecf9842d878f3633045f2a4858ace69
Ihor Klieshchov <igorklieshchov@Ihors-MacBook-Pro.local>
2020-07-15 23:35:50 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// ##
+// ##
+// ## Hi! Thanks for installing
+// ##
+// ## The code is coming later. Just holding the place =3
+// ##
+// ##
+// ## Best wishes,
+// ## Ihor 
+// ##
+// ##
+
+function whatIsTheAnswerToLifeTheUniverseAndEverything() {
+  return 42;
+}
